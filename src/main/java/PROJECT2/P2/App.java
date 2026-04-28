0d8ef229fe7392b1553696a41c29c128dc810033
A-Ankitha <avankitha.10a@gmail.com>
2026-04-28 05:46:34 +0530

    first commit

---
+++ src/main/java/PROJECT2/P2/App.java
@@ -0,0 +1,13 @@
+package PROJECT2.P2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
